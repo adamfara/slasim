2a2ed3a14f4f3db60ea5872ce80d8cc768a373b6
Adam Farabaugh <adamfara@seas.upenn.edu>
2015-05-24 17:27:45 -0400

    bellcrank orientation and circle intersection generalization

--- sla_kinematics.m
+++ sla_kinematics.m
@@ -64,38 +64,45 @@ shock_inboard = ipts(20,:);
 
 %% get closest point on upper a-arm axis to outer bj by projection
 g = upper_fibj - upper_ribj;
-g_unit = g/(sqrt(sum(g.^2)));
+g_unit = g / norm(g);
 v = upper_obj - upper_ribj;
-cp_upper_offset = dot(g_unit,v);
-cp_upper = upper_ribj + cp_upper_offset*g_unit;
+cp_upper_offset = dot(g_unit, v);
+cp_upper = upper_ribj + cp_upper_offset * g_unit;
 
 upper_vector = upper_obj - cp_upper;
-upper_length = norm(upper_vector, 2);
+upper_length = norm(upper_vector);
 
 
 %% get closest point on lower a-arm axis to outer bj by projection
 u = lower_fibj - lower_ribj;
-u_unit = u/(sqrt(sum(u.^2)));
+u_unit = u / norm(u);
 v = lower_obj - lower_ribj;
-cp_lower_offset = dot(u_unit,v);
-cp_lower = lower_ribj + cp_lower_offset*u_unit;
+cp_lower_offset = dot(u_unit, v);
+cp_lower = lower_ribj + cp_lower_offset * u_unit;
 
 lower_vector = lower_obj - cp_lower;
-lower_length = norm(lower_vector, 2);
+lower_length = norm(lower_vector);
 
 
-%%
+%% get closest point on arb axis to arb link point
+r = arb_pivot - arb_axis;
+r_unit = r ./ norm(r);
+v = arb_link_pt_init - arb_pivot;
+cp_arb_offset = dot(r_unit, v);
+cp_arb = arb_link_pt_init + cp_arb_offset * r_unit;
+
+
+%% Step size discretization
 step = -direction*(travel/2) / n_step;
 
 
-%% fixed distance between outer ball joints
+%% Fixed distance between outer ball joints
 w = upper_obj - lower_obj;
 dist_obj = norm(w,2);
 
 
 %% Numerically construct a circle around the A-arms
 theta = linspace(0, 2*pi, n_points);
-% theta2 = linspace(-pi/4, pi/4, n_points);
 t1 = null(u_unit);
 t2 = null(g_unit);
 a = t1(:,1);
@@ -180,33 +187,10 @@ for i = 2:n_step
     %Lower OBJ location is the point with the lowest distance error
     P_3D(i,:) = lower_circ(index,:);
     
-    
-    %Find the intersection of the circle described by the upper A-arm OBJ
-    %with the sphere defined by the distance between the lower A-arm OBJ at
-    %its center and a radius equal to the length between the ball joints.
-    %Since this will generally have two solutions, the index is found for
-    %both cases.
-    err2 = bsxfun(@minus, sqrt(sum(bsxfun(@minus, upper_circ, P_3D(i,:)).^2,2)), dist_obj);
-    mins = find(imregionalmin(abs(err2)));
-    mins(mins == 1) = [];
-    mins(mins == n_points) = [];
-    
-    min_index_inc = mins(1);
-    min_index_dec = mins(2);
-    
-    %In order to find the 'correct' intersection, the program looks to the
-    %last solution found. Whichever intersection point is closer to the
-    %last solution will be the correct one.
-    point_inc = upper_circ(min_index_inc,:);
-    point_dec = upper_circ(min_index_dec,:);
-    dist_inc = norm((point_inc - Q_3D(i-1,:)),2);
-    dist_dec = norm((point_dec - Q_3D(i-1,:)),2);
-    if dist_inc < dist_dec
-        Q_3D(i,:) = point_inc;
-    else
-        Q_3D(i,:) = point_dec;
-    end
-%     disp(min(abs(err2(min_index_inc)), abs(err2(min_index_dec))));
+    %Search through the discretizated circle for Upper OBJ location 
+    [Qtemp, minQerr] = find_intersection_circle_sphere( upper_circ, P_3D(i,:), Q_3D(i-1,:), dist_obj);
+    Q_3D(i,:) = Qtemp;
+    disp(minQerr);
 
     %Finding tie rod outer ball joint location (i.e., coupler rotation)
     %Normalized (unit) vector of steering knuckle
@@ -222,40 +206,12 @@ for i = 2:n_step
     e = t3(:,1);
     f = t3(:,2);
     
-    %Construct the circle numerically (i.e., generate n_points on the
-    %circle)
     circle = bsxfun(@plus, close_point(i,:), (e*cos(theta)*r_circ + f*sin(theta)*r_circ)');
-    dist_err = bsxfun(@minus, sqrt(sum(bsxfun(@minus, circle, tierod_inner).^2, 2)), ltierod);
-    
-    %Finding indices of minimum dist_err. Since the intersection of a
-    %sphere and circle is generally two points, there will be two minima
-    
-    mins = find(imregionalmin(abs(dist_err)));
-    mins(mins == 1) = [];
-    mins(mins == n_points) = [];
-    min_index_inc = mins(1);
-    min_index_dec = mins(2);
-    
-    %Determine which of the two solutions is correct by checking which
-    %defines a point closer to the last value
-    tobj_inc = circle(min_index_inc(1),:,1);
-    tobj_dec = circle(min_index_dec(1),:,1);
-    dist_inc = norm((tobj_inc - tierod_obj(i-1,:)),2);
-    dist_dec = norm((tobj_dec - tierod_obj(i-1,:)),2);
-    if dist_inc < dist_dec
-        tierod_obj(i,:) = circle(min_index_inc,:);
-    else
-        tierod_obj(i,:) = circle(min_index_dec,:);
-    end
-    
-    %Next, use transformation matrices to find the location of the wheel
-    %center and contact patch. Since the wheel center and contact patch are
-    %fixed w.r.t. the steering knuckle, the transformation matrix along
-    %with the orientation of the knuckle uniquely describe the contact
-    %patch and wheel center locations.
+    [tobjtemp, minTRerr] = find_intersection_circle_sphere(circle, tierod_inner, tierod_obj(i-1,:), ltierod);
+    tierod_obj(i,:) = tobjtemp;
+    disp(minTRerr);
     
     if i == 2
-        
         %The first time through, need to find the position of the wheel center
         %and contact patch w.r.t. the knuckle coordinate system. Since this
         %does not change, it only needs to be calculated once.
@@ -293,31 +249,10 @@ for i = 2:n_step
     pushrod_outer(i,:) = apply_kcs(la_kcs, P_3D(i,:), lower_ribj, lower_fibj);
     
     
-    %Find the intersection of the circle described by the bellcrank pr pt
-    %with the sphere defined by the distance between the pushrod OBJ at
-    %its center and a radius equal to the length between the ball joints.
-    err3 = bsxfun(@minus, sqrt(sum(bsxfun(@minus, bc_circ_pr, pushrod_outer(i,:)).^2,2)), pushrod_length);
-    mins = find(imregionalmin(abs(err3)));
-%     keyboard;
-    mins(mins == 1) = [];
-    mins(mins == n_points) = [];
-    
-    min_index_inc = mins(1);
-    min_index_dec = mins(2);
-    
-    %In order to find the 'correct' intersection, the program looks to the
-    %last solution found. Whichever intersection point is closer to the
-    %last solution will be the correct one.
-    point_inc = bc_circ_pr(min_index_inc,:);
-    point_dec = bc_circ_pr(min_index_dec,:);
-    dist_inc = norm(point_inc - pushrod_outer(i-1,:));
-    dist_dec = norm(point_dec - pushrod_outer(i-1,:));
-    if dist_inc < dist_dec
-        bellcrank_pr(i,:) = point_inc;
-    else
-        bellcrank_pr(i,:) = point_dec;
-    end
-    disp(min(abs(err3(min_index_inc)), abs(err3(min_index_dec))));
+    % Find bellcrank orientation with pr length
+    [bcprtemp, minBCPRerr] = find_intersection_circle_sphere(bc_circ_pr, pushrod_outer(i,:), bellcrank_pr(i-1,:), pushrod_length);
+    bellcrank_pr(i,:) = bcprtemp;
+    disp(minBCPRerr);
     
     %Solve for bellcrank shock pt position
     bellcrank_shock(i,:) = apply_kcs(bc_kcs_sh, bellcrank_pivot, bellcrank_pr(i,:), bellcrank_axis);
